e676f8252679fa6b839b1baa7b9ab2677ba18203
Z User <z@container>
2026-05-09 08:57:35 +0000

    46979717-f4b2-4228-b142-56e5147011aa

---
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/app/api/proxy/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/proxy/route.js")
+R.c("server/chunks/[externals]_next_dist_03fe02e0._.js")
+R.c("server/chunks/[root-of-the-server]__f408c708._.js")
+R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_6bc5d460.js")
+R.c("server/chunks/_next-internal_server_app_api_proxy_route_actions_caa765c7.js")
+R.m(69687)
+module.exports=R.m(69687).exports
